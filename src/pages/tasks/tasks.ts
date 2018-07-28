6eda7e9c55e5cceeff8b95d666a8c63692279539
Dylan Chong <dylan.chong@icloud.com>
2018-07-28 19:55:07 +1200

    Fixed task page to use new angular fire

--- src/pages/tasks/tasks.ts
+++ src/pages/tasks/tasks.ts
@@ -2,20 +2,23 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { UserService, Task } from '../../providers/user-service/user-service';
 import { EditTaskPage, EditTaskStrategy, AddTaskStrategy } from '../edit-task/edit-task';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'page-tasks',
   templateUrl: 'tasks.html',
 })
 export class TasksPage {
-
-  public tasks: Task[] = [];
+  public tasks: Observable<Task[]>;
 
   constructor(
     public navCtrl: NavController,
-    private userService: UserService,
+    userService: UserService,
   ) {
-    this.tasks = this.userService.tasks();
+    this.tasks = userService.user.pipe(map(
+      ({ tasks }) => tasks
+    ));
   }
 
   public onAddClicked() {
