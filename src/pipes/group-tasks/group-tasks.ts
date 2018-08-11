bb7d4743d99b51e18d5c8614676541f448a6f1ba
Dylan Chong <dylan.chong@icloud.com>
2018-08-11 13:06:12 +1200

    Rewrite groupings with moment js

--- src/pipes/group-tasks/group-tasks.ts
+++ src/pipes/group-tasks/group-tasks.ts
@@ -1,5 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Task } from '../../providers/user-service/user-service';
+import * as moment from 'moment';
 
 interface TaskPair {
   key: string;
@@ -21,32 +22,20 @@ export class GroupTasksPipe implements PipeTransform {
       return [];
     }
 
-    return groupers
+    const groups = groupers
       .map(grouper => grouper(tasks))
       .filter(group => group.tasks.length > 0);
+    return groups;
   }
 
 }
 
 // TODO move all of these functions into better places, considering layers
 
-// https://stackoverflow.com/a/43855221/1726450
-const isSameDay = (d1: Date, d2: Date) => {
-  return d1.getFullYear() === d2.getFullYear() &&
-    d1.getMonth() === d2.getMonth() &&
-    d1.getDate() === d2.getDate();
-};
-
 const isOverdue = (task: Task) => {
   const due = new Date(task.dueDate);
   return due.getTime() > Date.now();
 };
-
-const tomorrow = () => {
-  const date = new Date();
-  date.setDate(date.getDate() + 1);
-  return date;
-}
 
 const groupers: ((tasks: TaskPair[]) => TaskGroup)[] = [
   (taskPairs) => ({
@@ -58,22 +47,19 @@ const groupers: ((tasks: TaskPair[]) => TaskGroup)[] = [
   (taskPairs) => ({
     title: 'Due Later Today',
     tasks: taskPairs.filter(({ value: task }) => 
-      isSameDay(task.dueDate, new Date()) && !isOverdue(task)
+      moment().isSame(task.dueDate, 'day') && !isOverdue(task)
     ),
   }),
   (taskPairs) => ({
     title: 'Due Tomorrow',
     tasks: taskPairs.filter(({ value: task }) => 
-      isSameDay(task.dueDate, tomorrow())
+      moment().add(1, 'days').isSame(task.dueDate, 'day')
     ),
   }),
   (taskPairs) => ({
     title: 'Due Some Other Time',
     tasks: taskPairs.filter(({ value: task }) => 
-      !isSameDay(task.dueDate, tomorrow()) 
-      && task.dueDate.getTime() > tomorrow().getTime()
+      moment().add(1, 'days').isBefore(task.dueDate, 'day')
     ),
   }),
 ];
-
-// TODO make tests instances
